5bf55faea43ce83276792be21b0cb92d2a0c056f
ParsaGooya <parsa.gooya@ec.gc.ca>
2026-06-23 19:05:13 +0000

    bug fix

--- cccma_ppp/data_modules/dataset/config_abc.py
+++ cccma_ppp/data_modules/dataset/config_abc.py
@@ -82,22 +82,22 @@ class DatasetConfigABC(abc.ABC):
                     raise ValueError('Condition data should be available'
                     ' on the same lead_times as model data.')
 
-                if hasattr(self, 'observation'):
-                    if self.observation is not None:
-                        if not self.condition.info.coords["lat"].equals(
-                                self.model.info.coords["lat"]
-                            ):
-                            raise ValueError(
-                                "model and condition data do not have the same latitudes cooridnates." /
-                                "when bias correcting to observations"
-                            )
-                        if not self.condition.info.coords["lon"].equals(
-                            self.model.info.coords["lon"]
-                            ):
-                            raise ValueError(
-                                "model and condition data do not have the same longitudes cooridnates." /
-                                "when bias correcting to observations"
-                            )
+            if hasattr(self, 'observation'):
+                if self.observation is not None:
+                    if not self.condition.info.coords["lat"].equals(
+                            self.model.info.coords["lat"]
+                        ):
+                        raise ValueError(
+                            "model and condition data do not have the same latitudes cooridnates." /
+                            "when bias correcting to observations"
+                        )
+                    if not self.condition.info.coords["lon"].equals(
+                        self.model.info.coords["lon"]
+                        ):
+                        raise ValueError(
+                            "model and condition data do not have the same longitudes cooridnates." /
+                            "when bias correcting to observations"
+                        )
                 
 
     @final
