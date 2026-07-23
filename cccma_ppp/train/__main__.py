22caf2aee6fad67346f4334eccaa4967fad5ba9c
rna002 <nehan.arora@ec.gc.ca>
2026-07-23 16:37:50 +0000

    Implemented monitoring module into main

--- cccma_ppp/train/__main__.py
+++ cccma_ppp/train/__main__.py
@@ -1,6 +1,23 @@
+from cccma_ppp.generic.monitoring import monitor
 from cccma_ppp.train.train import main, get_parser
 
-if __name__ == "__main__":
-    parser = get_parser()
-    args = parser.parse_args()
-    main(args.config)
+m = monitor(cpu=True, ram=True, gpus=[0, 1], interval=0.1)
+m.start()
+
+try:
+    if __name__ == "__main__":
+        with m.span("Running Pipeline"):
+            parser = get_parser()
+            args = parser.parse_args()
+            main(args.config)
+finally:
+    m.stop()
+
+    df = m.get_dataframe()
+
+    m.plot(
+        df,
+        save_path="scripts/resource_usage.png",
+        show=False,
+        smooth="kalman",
+    )
