d69bf3827c1bbbb46462ba476f434960ca1e4a1b
rna002 <nehan.arora@ec.gc.ca>
2026-07-30 15:57:43 +0000

    Removed unused files

--- cccma_ppp/inference/registry_imports.py
+++
@@ -1,7 +0,0 @@
-import cccma_ppp.models.mlp_models  # noqa: F401
-import cccma_ppp.core.cVAE_module  # noqa: F401
-import cccma_ppp.core.deterministic_module  # noqa: F401
-import cccma_ppp.loss.utils_loss  # noqa: F401
-import cccma_ppp.preprocessing.utils_preprocessing  # noqa: F401
-from cccma_ppp.models.normalized_flows import MAF  # noqa: F401
-from cccma_ppp.models.normalized_flows import RealNVP  # noqa: F401
