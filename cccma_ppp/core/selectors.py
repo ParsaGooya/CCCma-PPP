f1fd3585bfab48abb4a8ca16695f699905beb8a4
ParsaGooya <parsa.gooya@ec.gc.ca>
2026-07-31 00:56:24 +0000

    removed unused import

--- cccma_ppp/core/selectors.py
+++ cccma_ppp/core/selectors.py
@@ -10,7 +10,6 @@ import warnings
 from cccma_ppp.core.registery import Registery
 from cccma_ppp.core.core_abc import moduleABC
 from cccma_ppp.models.models_abc import modelABC, flowABC, CheckpointConfig
-from cccma_ppp.generic import Distributed
 
 @dataclasses.dataclass
 class ModuleSelector:
