13be4088323c24afc305fe2b6aa499f054ea5c78
rna002 <nehan.arora@ec.gc.ca>
2026-08-17 16:49:37 +0000

    Remove formatting checks for registry_imports

--- cccma_ppp/generic/registry_imports.py
+++ cccma_ppp/generic/registry_imports.py
@@ -1,8 +1,8 @@
-import cccma_ppp.architectures.mlp              
-import cccma_ppp.architectures.unet              
-import cccma_ppp.core.models.cvae              
-import cccma_ppp.core.models.deterministic              
-import cccma_ppp.loss.utils_loss              
-import cccma_ppp.preprocessing.utils_preprocessing              
-from cccma_ppp.architectures.normalized_flows import MAF              
-from cccma_ppp.architectures.normalized_flows import RealNVP              
+import cccma_ppp.architectures.mlp  # noqa: F401
+import cccma_ppp.architectures.unet  # noqa: F401
+import cccma_ppp.core.models.cvae  # noqa: F401
+import cccma_ppp.core.models.deterministic  # noqa: F401
+import cccma_ppp.loss.utils_loss  # noqa: F401
+import cccma_ppp.preprocessing.utils_preprocessing  # noqa: F401
+from cccma_ppp.architectures.normalized_flows import MAF  # noqa: F401
+from cccma_ppp.architectures.normalized_flows import RealNVP  # noqa: F401
