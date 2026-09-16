f0c73ba7be8e686a934bc7338b027211122e3f10
ParsaGooya <parsa.gooya@ec.gc.ca>
2026-09-16 23:43:55 +0000

    set defaults

--- cccma_ppp/generic/runtime.py
+++ cccma_ppp/generic/runtime.py
@@ -27,7 +27,9 @@ class RuntimeContext:
         Description not yet provided.
     """
 
-    GLOBAL_EXP_DIR: Path | str | None = None
+    GLOBAL_EXP_DIR: Path | str | None = dataclasses.field(
+        default_factory=Path.cwd
+    )
     GLOBAL_CHECKPOINT_DIR: Path | str | None = None
     GLOBAL_FIGURES_DIR: Path | str | None = None
     GLOBAL_LOG_DIR: Path | str | None = None
@@ -36,3 +38,21 @@ class RuntimeContext:
 
     INPUT_VAR_METADATA: dict | None = None
     TARGET_VAR_METADATA: dict | None = None
+
+    def __post_init__(self):
+
+        if self.GLOBAL_CHECKPOINT_DIR is None:
+            self.GLOBAL_CHECKPOINT_DIR = self.GLOBAL_EXP_DIR / "checkpoints"
+
+        if self.GLOBAL_FIGURES_DIR is None:
+            self.GLOBAL_FIGURES_DIR = self.GLOBAL_EXP_DIR / "figures"
+
+        if self.GLOBAL_LOG_DIR is None:
+            self.GLOBAL_LOG_DIR = self.GLOBAL_EXP_DIR / "logs"
+
+        if self.GLOBAL_MONITORING_DIR is None:
+            self.GLOBAL_MONITORING_DIR = self.GLOBAL_EXP_DIR / "resource_monitoring"
+
+        if self.GLOBAL_OUTPUT_DIR is None:
+            self.GLOBAL_OUTPUT_DIR = self.GLOBAL_EXP_DIR / "inference"
+        
